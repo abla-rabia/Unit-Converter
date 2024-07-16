410b2443b0ede04a0a0239bb556160fabc372d66
abla-rabia <la_rabia@esi.dz>
2024-07-16 14:47:04 +0100

    adding ui elements

--- app/src/main/java/ar/android/unitconverter/MainActivity.kt
+++ app/src/main/java/ar/android/unitconverter/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ar.android.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,28 +43,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(){
-    Column {
-        Greeting(name = "Abla")
-        Greeting(name = "Rabia")
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
         Row {
-            Greeting(name = "Ablus")
-            Greeting(name = "Ablus")
+            Text("Unit Converter")
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Box(){
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = {Text("Centimeters")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text("Meters")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text("Feets")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text("Milimeters")}, onClick = { /*TODO*/ })
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box(){
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = {Text("Centimeters")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text("Meters")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text("Feets")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text("Milimeters")}, onClick = { /*TODO*/ })
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Result")
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
-    }
+fun UniConverterPreview() {
+    UnitConverter()
 }
